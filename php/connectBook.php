f49a754608cd74f9e521afae10cd6789a3e5bbe6
rock051091 <rock051091@gmail.com>
2020-11-04 00:12:55 +0800

    blog in php

--- php/connectBook.php
+++ php/connectBook.php
@@ -1,8 +1,7 @@
-<?php
-    $dbname='ed103g1';
-    $dsn= "mysql:host=127.0.0.1;port=3306;dbname=$dbname;charset=utf8";
-    $user= "ed103g1";
-    $password= "ed103g1";
-    $options= array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
-    $pdo= new PDO($dsn, $user, $password, $options);
-?> 
+<?php 
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=ed103;charset=utf8";
+$user = "root";
+$password = "root";
+$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
+$pdo = new PDO($dsn, $user, $password, $options);
+?>   
