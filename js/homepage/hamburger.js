0eb3e3c9bf8fd022e652c19b393461cd937af43e
BoXiang <a123789225@gmail.com>
2020-11-06 20:49:02 +0800

    柏翔放1106-(新版登入)完成

--- js/homepage/hamburger.js
+++
@@ -1,6 +0,0 @@
-//Hamburger Btn
-$("div.hamburger_box").on("click", function () {
-    $("span.hamburger_line").toggleClass("is-active");
-    $("span.hamburger_cross").toggleClass("is-active");
-    $("ul.main_menu_ul").toggleClass("is-active");
-});
