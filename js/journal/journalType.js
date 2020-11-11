890c0d3770bf8429e8ad385aa6e3e9669d0c4559
christy0408 <c19930408@gmail.com>
2020-11-11 13:53:54 +0800

    1111 Happy single day~~~

--- js/journal/journalType.js
+++ js/journal/journalType.js
@@ -1,10 +1,12 @@
 function chooseAquaType(){
     //=====使用Ajax 回server端,取回aquatype, 放到頁面上 
+
+// Dolphin
     let xhr = new XMLHttpRequest();
 
     xhr.onload = function(){
-        aquas = JSON.parse(xhr.responseText);
-        var bookCoverHTML='';
+        let aquas = JSON.parse(xhr.responseText);
+        let bookCoverHTML='';
         for(i=0; i<aquas.length; i++){
             bookCoverHTML += `
 
@@ -52,16 +54,207 @@ function chooseAquaType(){
             </div>
         `;
         }
-        let jourBooks = document.getElementById('jourBooks');
+        let jourBooks = document.getElementById('jourBooksDolphin');
         jourBooks.innerHTML = bookCoverHTML;
         
     }
-    xhr.open("Get", "journalType.php", true);
+    xhr.open("Get", "journalTypeDolphin.php", true);
     xhr.send(null); 
+
+
+// Whale
+    let xhrWhale = new XMLHttpRequest();
+
+    xhrWhale.onload = function(){
+        let aquas = JSON.parse(xhrWhale.responseText);
+        let bookCoverHTML='';
+        for(i=0; i<aquas.length; i++){
+            bookCoverHTML += `
+
+            <div class="jourBookSection" id="jourBookSection">
+                <a href="#">
+
+                <div class="jourBook jourBookOpenBtn" id="jourBookOpenBtn">
+                    <div class="jourBookLeft">
+
+                    </div>
+                    <div class="jourBookRight">
+                        <div class="jourBookPhoto">
+                            <img src="././image/journal/journal_whale/00.jpg" alt="">
+                        </div>
+                    <div class="jourBookName">${aquas[i].aquaName}</div>
+                        <div class="jourBookText">
+                            <table>
+                            <tr>
+                                <th>Aqua No.</th>
+                                <td>${aquas[i].aquaNo}</td>
+                            </tr>
+                            <tr>
+                                <th>Aqua Name</th>
+                                <td>${aquas[i].aquaName}</td>
+                            </tr>
+                            <tr>
+                                <th>Found Location</th>
+                                <td>${aquas[i].rescueLoc}</td>
+                            </tr>
+                            <tr>
+                                <th>Arrival Date</th>
+                                <td>${aquas[i].arriveDate}</td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                </a>
+                <!-- 陰影 -->
+                <div class="jourBookShadow"></div>
+                <!-- 石頭 -->
+                <div class="jourStone">
+                <img src="./image/journal/journal_element/stone.png" alt="">
+                </div>
+            </div>
+        `;
+        }
+        
+        let jourBooks = document.getElementById('jourBooksWhale');
+        jourBooks.innerHTML = bookCoverHTML;
+        
+    }
+    xhrWhale.open("Get", "journalTypeWhale.php", true);
+    xhrWhale.send(null); 
+
+// Seal
+let xhrSeal = new XMLHttpRequest();
+
+xhrSeal.onload = function(){
+    let aquas = JSON.parse(xhrSeal.responseText);
+    let bookCoverHTML='';
+    for(i=0; i<aquas.length; i++){
+        bookCoverHTML += `
+
+        <div class="jourBookSection" id="jourBookSection">
+            <a href="#">
+
+            <div class="jourBook jourBookOpenBtn" id="jourBookOpenBtn">
+                <div class="jourBookLeft">
+
+                </div>
+                <div class="jourBookRight">
+                    <div class="jourBookPhoto">
+                        <img src="././image/journal/journal_Seal/00.jpg" alt="">
+                    </div>
+                <div class="jourBookName">${aquas[i].aquaName}</div>
+                    <div class="jourBookText">
+                        <table>
+                        <tr>
+                            <th>Aqua No.</th>
+                            <td>${aquas[i].aquaNo}</td>
+                        </tr>
+                        <tr>
+                            <th>Aqua Name</th>
+                            <td>${aquas[i].aquaName}</td>
+                        </tr>
+                        <tr>
+                            <th>Found Location</th>
+                            <td>${aquas[i].rescueLoc}</td>
+                        </tr>
+                        <tr>
+                            <th>Arrival Date</th>
+                            <td>${aquas[i].arriveDate}</td>
+                        </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            </a>
+            <!-- 陰影 -->
+            <div class="jourBookShadow"></div>
+            <!-- 石頭 -->
+            <div class="jourStone">
+            <img src="./image/journal/journal_element/stone.png" alt="">
+            </div>
+        </div>
+    `;
+    }
+    
+    let jourBooks = document.getElementById('jourBooksSeal');
+    jourBooks.innerHTML = bookCoverHTML;
+    
+}
+xhrSeal.open("Get", "journalTypeSeal.php", true);
+xhrSeal.send(null); 
+
+
+// Turtle
+let xhrTurtle = new XMLHttpRequest();
+
+xhrTurtle.onload = function(){
+    let aquas = JSON.parse(xhrTurtle.responseText);
+    let bookCoverHTML='';
+    for(i=0; i<aquas.length; i++){
+        bookCoverHTML += `
+
+        <div class="jourBookSection" id="jourBookSection">
+            <a href="#">
+
+            <div class="jourBook jourBookOpenBtn" id="jourBookOpenBtn">
+                <div class="jourBookLeft">
+
+                </div>
+                <div class="jourBookRight">
+                    <div class="jourBookPhoto">
+                        <img src="././image/journal/journal_turtle/00.jpg" alt="">
+                    </div>
+                <div class="jourBookName">${aquas[i].aquaName}</div>
+                    <div class="jourBookText">
+                        <table>
+                        <tr>
+                            <th>Aqua No.</th>
+                            <td>${aquas[i].aquaNo}</td>
+                        </tr>
+                        <tr>
+                            <th>Aqua Name</th>
+                            <td>${aquas[i].aquaName}</td>
+                        </tr>
+                        <tr>
+                            <th>Found Location</th>
+                            <td>${aquas[i].rescueLoc}</td>
+                        </tr>
+                        <tr>
+                            <th>Arrival Date</th>
+                            <td>${aquas[i].arriveDate}</td>
+                        </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            </a>
+            <!-- 陰影 -->
+            <div class="jourBookShadow"></div>
+            <!-- 石頭 -->
+            <div class="jourStone">
+            <img src="./image/journal/journal_element/stone.png" alt="">
+            </div>
+        </div>
+    `;
+    }
+    
+    let jourBooks = document.getElementById('jourBooksTurtle');
+    jourBooks.innerHTML = bookCoverHTML;
+    
+}
+xhrTurtle.open("Get", "journalTypeTurtle.php", true);
+xhrTurtle.send(null); 
+
+
 }
 
 
 // }
+
+
+
+
 
 
 window.addEventListener('load', chooseAquaType);
