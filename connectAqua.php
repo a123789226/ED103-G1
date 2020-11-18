96884009fe9d16ccb920f1e15ce4af500fd9495b
marco52613 <marco52613@hotmail.com>
2020-11-18 16:39:00 +0800

    blog收藏

--- connectAqua.php
+++ connectAqua.php
@@ -1,10 +1,8 @@
 <?php 
 // SQ專用
-<<<<<<< HEAD
+
 $dsn = "mysql:host=localhost;port=3306;dbname=ed103g1;charset=utf8";
-=======
-$dsn = "mysql:host=127.0.0.1;port=3306;dbname=ed103g1;charset=utf8";
->>>>>>> SQ
+
 $user = "root";
 $password = "root";
 $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
