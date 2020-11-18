47eaa3afd9cf6529fb21f094421f4bf77120013e
Square.Chang <ctrla0730@me.com>
2020-11-18 10:25:18 +0800

    公版connectAqua

--- connectAqua.php
+++ connectAqua.php
@@ -1,8 +1,8 @@
 <?php 
 // SQ專用
 $dsn = "mysql:host=localhost;port=3306;dbname=ed103g1;charset=utf8";
-$user = "root";
-$password = "root";
+$user = "ed103g1";
+$password = "ed103g1";
 $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
 $pdo = new PDO($dsn, $user, $password, $options);
 ?>
