b377dc387bd54e04ceffc7b35c2b549233331695
marco52613 <marco52613@hotmail.com>
2020-11-20 22:11:35 +0800

    1120

--- js/journal/adopt.js
+++ js/journal/adopt.js
@@ -6,46 +6,46 @@ function doFirst(){
         amountResult.innerText = adoptAmount.value;
     });
 
-    //First Name
-    let adoptFirstname = document.getElementById('adoptFirstname');
-    let adoptFirstnameResult = document.getElementById('adoptFirstnameResult');
+    // //First Name
+    // let adoptFirstname = document.getElementById('adoptFirstname');
+    // let adoptFirstnameResult = document.getElementById('adoptFirstnameResult');
 
-    adoptFirstname.addEventListener('blur', function(){
-        adoptFirstnameResult.innerText = adoptFirstname.value;
-    });
+    // adoptFirstname.addEventListener('blur', function(){
+    //     adoptFirstnameResult.innerText = adoptFirstname.value;
+    // });
 
 
-    //Last Name
-    let adoptLastname = document.getElementById('adoptLastname');
-    let adoptLastnameResult = document.getElementById('adoptLastnameResult');
+    // //Last Name
+    // let adoptLastname = document.getElementById('adoptLastname');
+    // let adoptLastnameResult = document.getElementById('adoptLastnameResult');
 
-    adoptLastname.addEventListener('blur', function(){
-        adoptLastnameResult.innerText = adoptLastname.value;
-    });
+    // adoptLastname.addEventListener('blur', function(){
+    //     adoptLastnameResult.innerText = adoptLastname.value;
+    // });
 
-     //Email
-     let adoptEmail = document.getElementById('adoptEmail');
-     let adoptEmailResult = document.getElementById('adoptEmailResult');
+    //  //Email
+    //  let adoptEmail = document.getElementById('adoptEmail');
+    //  let adoptEmailResult = document.getElementById('adoptEmailResult');
  
-     adoptEmail.addEventListener('blur', function(){
-        adoptEmailResult.innerText = adoptEmail.value;
-     });
+    //  adoptEmail.addEventListener('blur', function(){
+    //     adoptEmailResult.innerText = adoptEmail.value;
+    //  });
 
-    //Phone
-     let adoptPhone = document.getElementById('adoptPhone');
-     let adoptPhoneResult = document.getElementById('adoptPhoneResult');
+    // //Phone
+    //  let adoptPhone = document.getElementById('adoptPhone');
+    //  let adoptPhoneResult = document.getElementById('adoptPhoneResult');
  
-     adoptPhone.addEventListener('blur', function(){
-        adoptPhoneResult.innerText = adoptPhone.value;
-     });
+    //  adoptPhone.addEventListener('blur', function(){
+    //     adoptPhoneResult.innerText = adoptPhone.value;
+    //  });
 
-      //CardName
-      let adoptCardname = document.getElementById('adoptCardname');
-      let adoptCardnameResult = document.getElementById('adoptCardnameResult');
+    //   //CardName
+    //   let adoptCardname = document.getElementById('adoptCardname');
+    //   let adoptCardnameResult = document.getElementById('adoptCardnameResult');
   
-      adoptCardname.addEventListener('blur', function(){
-        adoptCardnameResult.innerText = adoptCardname.value;
-      });
+    //   adoptCardname.addEventListener('blur', function(){
+    //     adoptCardnameResult.innerText = adoptCardname.value;
+    //   });
 
        //CardNumber
        let adoptCardnumber = document.getElementById('adoptCardnumber');
