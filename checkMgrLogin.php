8f7a4577046a847643ff0c1f2ec26deb3f5d17aa
DESKTOP-OVP520O\Lin <marco52613@hotmail.com>
2020-11-22 01:51:03 +0800

    新增後台若session無管理者登入紀錄，或管理者已被停權，強制跳轉回登入頁面

--- checkMgrLogin.php
+++ checkMgrLogin.php
@@ -1,13 +1,55 @@
 <?php 
 // 登入直接抓取session的資料
+// session_start();
+// if(isset($_SESSION["mgrId"]) === true){
+// 	$result = array("mgrId"=>$_SESSION["mgrId"],
+// 									"mgrStatus"=>$_SESSION["mgrStatus"]);
+// 	echo json_encode($result);
+// }else{
+// 	echo "{}";
+// }
+
 session_start();
 if(isset($_SESSION["mgrId"]) === true){
-	$result = array("mgrId"=>$_SESSION["mgrId"],
-									"mgrStatus"=>$_SESSION["mgrStatus"]);
-	echo json_encode($result);
+	try {
+		require("connectAqua.php");
+
+		$sql = "select * from `manager` where mgrId=:mgrId";
+		
+		$mgr = $pdo->prepare($sql);
+		$mgr->bindValue(":mgrId", $_SESSION["mgrId"]);
+		$mgr->execute();
+		
+		if($mgr->rowCount()==0){ // 查無此人
+			echo "{}";
+		}else{ // 登入成功
+			// 自資料庫中取回資料
+			$mgrRow = $mgr->fetch(PDO::FETCH_ASSOC);
+			// 將管理者資料重新寫入session
+      $_SESSION["mgrNo"] = $mgrRow["mgrNo"];
+      $_SESSION["mgrName"] = $mgrRow["mgrName"];
+      $_SESSION["mgrId"] = $mgrRow["mgrId"];
+      $_SESSION["mgrPsw"] = $mgrRow["mgrPsw"];
+      $_SESSION["mgrStatus"] = $mgrRow["mgrStatus"];
+
+      $result = array("mgrNo"=>$mgrRow["mgrNo"],
+                      "mgrName"=>$mgrRow["mgrName"],
+                      "mgrId"=>$mgrRow["mgrId"],
+                      "mgrPsw"=>$mgrRow["mgrPsw"],
+                      "mgrStatus"=>$mgrRow["mgrStatus"]);
+      
+			$json = json_encode($result);
+			echo $json;
+		}
+	}catch(PDOException $e){  
+		$error = array("errorMsg"=>$e->getMessage());
+		echo json_encode($error);//{"errorMsg":"......."}
+	}
+
 }else{
 	echo "{}";
 }
+
 ?>
 
 
